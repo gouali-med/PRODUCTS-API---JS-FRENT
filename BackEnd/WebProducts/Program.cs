c4dd3dce3b0db11778916a5faa9c121d3f1adc26
gouali-med <mohammed.gouali@gmail.com>
2023-09-11 14:15:19 +0100

    first commit

---
+++ BackEnd/WebProducts/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System;
+using WebProducts.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(option => 
+    option.UseSqlServer(builder.Configuration.GetConnectionString("ProductConnection")));
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy", buil =>
+  {
+      buil.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+     
+  }));
+
+
+
+//builder.Services.AddSingleton<IAuthorizationHandler, MyHandler1>();
+//// MyHandler2, ...
+
+//builder.Services.AddSingleton<IAuthorizationHandler, MyHandlerN>();
+
+//// Configure your policies
+//builder.Services.AddAuthorization(options =>
+//      options.AddPolicy("Something",
+//      policy => policy.RequireClaim("Permission", "CanViewPage", "CanViewAnything")));
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors("CorsPolicy");
+app.Run();
